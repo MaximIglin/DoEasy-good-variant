75f62b1f947379a86c9cac52fa756ef676b8dbd7
lehnele <lehnele@gmail.com>
2021-09-13 22:29:59 +0300

    users_page

--- easy-choice-ui/src/pages/users_system/user_page.jsx
+++ easy-choice-ui/src/pages/users_system/user_page.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import { useState, useEffect } from "react/cjs/react.development";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import classes from "./user_page.module.css"
 
 const UserPage = () => {
 
@@ -29,7 +30,53 @@ const UserPage = () => {
             <Header/>
             {isLoaded
             ? <div>Идёт загрузка</div>
-            : <div>{user.email}</div>
+            : <div className={classes.account}>
+                    <div className={classes.account_content}>
+                        <div className={classes.accaunt_title}>
+                            <div className={classes.accaunt_avatar}>
+                                <img src="http://pm1.narvii.com/6526/85c4320891c9f6162769969d9c770b3feeb7246d_00.jpg" alt="" />
+                            </div>
+                        </div>
+                            <div className={classes.accaunt_name}>{user.first_name} {user.last_name} </div>
+                            <div className={classes.accaunt_info}>
+
+                            <div  className={classes.info_component}>
+                                <div className={classes.key}>Никнейм</div> 
+                                <div className={classes.value_and_btn}>
+                                    <div>{user.username}</div>
+                                    <div className={classes.btn}><img src="https://image.flaticon.com/icons/png/512/565/565722.png" alt="" /></div>
+                                </div>
+                                
+                            </div>
+
+                            <div className={classes.info_component}>
+                                <div className={classes.key}>Номер</div>
+                                <div className={classes.value_and_btn}>
+                                        <div>8-800-535-35-35</div>
+                                        <div className={classes.btn}><img src="https://image.flaticon.com/icons/png/512/565/565722.png" alt="" /></div>
+                                </div>
+                            </div>
+
+                            <div className={classes.info_component}>
+                                <div className={classes.key}>Email</div>
+                                <div className={classes.value_and_btn}>
+                                    <div>{user.email}</div>
+                                    <div className={classes.btn}><img src="https://image.flaticon.com/icons/png/512/565/565722.png" alt="" /></div>
+                                </div>
+                            </div>
+
+                            <div className={classes.info_component}>
+                                <div className={classes.key}>Дата рождения</div>
+                                <div className={classes.value_and_btn}>
+                                        <div>{user.birh_date}</div>
+                                        <div className={classes.btn}><img src="https://image.flaticon.com/icons/png/512/565/565722.png" alt="" /></div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            
             }           
             <Footer/>
         </div>
